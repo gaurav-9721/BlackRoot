c98534b6934803455d5a72b7b698c25ab2883d1e
gaurav-9721 <vermagourav2001@gmail.com>
2022-10-08 01:08:11 +0530

    Init Game

--- static/main.js
+++ static/main.js
@@ -1,26 +1,26 @@
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+let engine = Matter.Engine.create();
 
-var engine = Engine.create();
+let render = Matter.Render.create({
+  element: document.body,
+  engine:engine
+});
+//parms: x,y,width,height,options
+let ground = Matter.Bodies.rectangle(400,600,810,60,{ isStatic: true});
+let boxA = Matter.Bodies.rectangle(400,200,80,80);
+let boxB = Matter.Bodies.rectangle(450,50,80,80);
 
-var render = Render.create({
-                element: document.body,
-                engine: engine,
-                options: {
-                    width: 800,
-                    height: 400,
-                    wireframes: false
-                }
-             });
+//Matter.World.add(engine.world,[boxA,boxB,ground,mouseConstraint]);
+Matter.Engine.run(engine);
+Matter.Render.run(render);
 
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var ballA = Bodies.circle(380, 100, 40, 10);
-var ballB = Bodies.circle(460, 10, 40, 10);
-var ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
+let mouse = Matter.Mouse.create(render.canvas);
+let mouseConstraint = Matter.MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    render: {visible: false}
+  }
+});
+render.mouse = mouse;
 
-World.add(engine.world, [boxA, ballA, ballB, ground]);
+Matter.World.add(engine.world,[boxA,boxB,ground,mouseConstraint]);
 
-Engine.run(engine);
-Render.run(render);
